6a9e07e26b1a5aa2511ad2de4aeb012975634ff7
Alexis Hevia <hevia.alexis@gmail.com>
2013-08-31 13:10:30 -0500

    initial commit

---
+++ README.md
@@ -0,0 +1,9 @@
+# Alexis Hevia's puppet files
+These puppet files should allow me to reconstruct my entire development environment on a blank PC
+
+## How To Use
+```
+sudo apt-get install puppet git
+git clone https://github.com/alexishevia/.puppet.git ~/.puppet
+puppet apply ~/.puppet/manifests/dotfiles.pp
+```
---
+++ manifests/dotfiles.pp
@@ -0,0 +1,57 @@
+vcsrepo { 'dotfiles':
+  path     => "/home/$id/.dotfiles",
+  ensure   => present,
+  provider => git,
+  source   => 'https://github.com/alexishevia/.dotfiles.git'
+}
+
+# VIM
+
+file { '.vim':
+  path   => "/home/$id/.vim",
+  ensure => link,
+  target => '.dotfiles/.vim',
+  require => Vcsrepo['dotfiles']
+}
+
+file { '.vimrc':
+  path   => "/home/$id/.vimrc",
+  ensure => link,
+  target => '.dotfiles/.vimrc',
+  require => Vcsrepo['dotfiles']
+}
+
+file { '.local.vimrc':
+  path   => "/home/$id/.local.vimrc",
+  ensure => link,
+  target => '.dotfiles/.local.vimrc.using_spaces',
+  require => Vcsrepo['dotfiles']
+}
+
+# GIT
+
+file { '.gitconfig':
+  path   => "/home/$id/.gitconfig",
+  ensure => link,
+  target => '.dotfiles/.gitconfig.personal',
+  require => Vcsrepo['dotfiles']
+}
+
+# Ack / Grep
+
+file { '.ackrc':
+  path   => "/home/$id/.ackrc",
+  ensure => link,
+  target => '.dotfiles/.ackrc',
+  require => Vcsrepo['dotfiles']
+}
+
+# Postgres
+
+file { '.psqlrc':
+  path   => "/home/$id/.psqlrc",
+  ensure => link,
+  target => '.dotfiles/.psqlrc',
+  require => Vcsrepo['dotfiles']
+}
+
