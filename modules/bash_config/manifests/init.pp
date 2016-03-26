f1ad0fb71f8d3b600baf4f6851a2e682301c92d2
Alexis Hevia <hevia.alexis@gmail.com>
2016-03-26 12:54:30 -0500

    make sure bashrc is always sourced

--- modules/bash_config/manifests/init.pp
+++ modules/bash_config/manifests/init.pp
@@ -7,8 +7,15 @@ class bash_config {
     source => "puppet:///modules/bash_config/.bashrc",
   }
 
-  file { ["/home/$id/.profile", "/home/$id/.bash_profile",
-          "/home/$id/.mkshrc", "/home/$id/.zshrc", "/home/$id/.zlogin"]:
+  file { "/home/$id/.bash_profile":
+    owner => "$id",
+    group => "$id",
+    mode => "644",
+    source => "puppet:///modules/bash_config/.bash_profile",
+  }
+
+  file { ["/home/$id/.profile", "/home/$id/.mkshrc",
+          "/home/$id/.zshrc", "/home/$id/.zlogin"]:
     ensure => "absent"
   }
 
