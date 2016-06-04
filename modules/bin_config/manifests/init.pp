47460ba3f5cba8851ea5f5246c48086b41eb32cc
Alexis Hevia <hevia.alexis@gmail.com>
2016-06-04 10:53:49 -0500

    add sp to ~/bin

--- modules/bin_config/manifests/init.pp
+++ modules/bin_config/manifests/init.pp
@@ -7,4 +7,11 @@ class bin_config {
     source => "puppet:///modules/bin_config/tmux_skyroam",
   }
 
+  file { "/home/$id/bin/sp":
+    owner => "$id",
+    group => "$id",
+    mode => "755",
+    source => "puppet:///modules/bin_config/sp",
+  }
+
 }
