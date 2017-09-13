8732375731ff4270f94bbe24809d2d5118720789
Alexis Hevia <hevia.alexis@gmail.com>
2017-09-13 09:06:21 -0500

    Add [skip ci] flag to commit message template

--- modules/git_config/manifests/init.pp
+++ modules/git_config/manifests/init.pp
@@ -7,4 +7,11 @@ class git_config {
     source => "puppet:///modules/git_config/.gitconfig",
   }
 
+  file { "/home/$id/.gitmessage":
+    owner => "$id",
+    group => "$id",
+    mode => "644",
+    source => "puppet:///modules/git_config/.gitmessage",
+  }
+
 }
